bed136ea1122b4f67c721e71e522b6c72f556fc9
devansh-ios <devansh.dev2592002@gmail.com>
2023-08-14 00:31:57 +0530

    make it responsive

--- src/component/Overlay/ConnectOverlay.js
+++ src/component/Overlay/ConnectOverlay.js
@@ -8,8 +8,8 @@ const ConnectOverlay = () => {
   return (
     <div className="socialContainer">
      <div><a href="https://www.linkedin.com/in/devansh-kumar-9798b21ba/" target="_blanck"> <LinkedInIcon sx={{ fontSize: 40 }}  /></a></div>
-     <div> <a href="https://www.instagram.com/__devansh_kumar_/" target="_blanck">><InstagramIcon sx={{ fontSize: 40 }} /></a></div>
-      <div><a href="mailTo:devansh.dev2592002@gmail.com" target="_blanck">><EmailIcon sx={{ fontSize: 40 }} /></a></div>
+     <div> <a href="https://www.instagram.com/__devansh_kumar_/" target="_blanck"><InstagramIcon sx={{ fontSize: 40 }} /></a></div>
+      <div><a href="mailTo:devansh.dev2592002@gmail.com" target="_blanck"><EmailIcon sx={{ fontSize: 40 }} /></a></div>
 
     </div>
   );
